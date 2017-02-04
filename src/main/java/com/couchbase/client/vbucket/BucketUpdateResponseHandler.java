903f59aa367c952c180883648eb709884a8f94b1
Ben Poweski <bpoweski@gmail.com>
2017-02-04 16:06:32 -0600

    change netty version to 3.10.5-Final
    * comment out logging statement using removed methods

--- src/main/java/com/couchbase/client/vbucket/BucketUpdateResponseHandler.java
+++ src/main/java/com/couchbase/client/vbucket/BucketUpdateResponseHandler.java
@@ -97,14 +97,14 @@ public class BucketUpdateResponseHandler extends SimpleChannelUpstreamHandler {
     finerLog("STATUS: " + response.getStatus());
     finerLog("VERSION: " + response.getProtocolVersion());
 
-    if (!response.getHeaderNames().isEmpty()) {
-      for (String name : response.getHeaderNames()) {
-        for (String value : response.getHeaders(name)) {
-          finerLog("HEADER: " + name + " = " + value);
-        }
-      }
-      finerLog(System.getProperty("line.separator"));
-    }
+    // if (!response.getHeaderNames().isEmpty()) {
+    //   for (String name : response.getHeaderNames()) {
+    //     for (String value : response.getHeaders(name)) {
+    //       finerLog("HEADER: " + name + " = " + value);
+    //     }
+    //   }
+    //   finerLog(System.getProperty("line.separator"));
+    // }
 
     if (response.getStatus().getCode() == 200 && response.isChunked()) {
       readingChunks = true;
